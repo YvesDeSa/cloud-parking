637b732e1a168096a83c179fd91b6d23da8ab04b
Yves de Sá <yvesdsb98@gmail.com>
2022-08-21 18:49:20 -0300

    chose: adding swagger

--- src/main/java/one/digitalinnovation/parking/service/ParkingService.java
+++ src/main/java/one/digitalinnovation/parking/service/ParkingService.java
@@ -1,5 +1,6 @@
 package one.digitalinnovation.parking.service;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -8,6 +9,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
+import one.digitalinnovation.parking.exception.ParkingNotFoundException;
 import one.digitalinnovation.parking.model.Parking;
 
 @Service
@@ -15,18 +17,48 @@ public class ParkingService {
 
   private static Map<String, Parking> parkingMap = new HashMap();
 
-  static {
-    var id = getUUID();
-    Parking parking = new Parking(id, "DMS-1111", "SC", "CELTA", "PRETO");
-    parkingMap.put(id, parking);
-  }
-
   public List<Parking> findAll() {
     return parkingMap.values().stream().collect(Collectors.toList());
   }
 
   private static String getUUID() {
     return UUID.randomUUID().toString().replace("-", "");
+  }
+
+  public Parking findById(String id) {
+    Parking parking = parkingMap.get(id);
+    if (parking == null) {
+      throw new ParkingNotFoundException(id);
+    }
+    return parking;
+  }
+
+  public Parking create(Parking parkingCreate) {
+    String uuid = getUUID();
+    parkingCreate.setId(uuid);
+    parkingCreate.setEntryDate(LocalDateTime.now());
+    parkingMap.put(uuid, parkingCreate);
+    return parkingCreate;
+  }
+
+  public void delete(String id) {
+    findById(id);
+    parkingMap.remove(id);
+  }
+
+  public Parking update(String id, Parking parkingCreate) {
+    Parking parking = findById(id);
+    parking.setColor(parkingCreate.getColor());
+    parkingMap.replace(id, parking);
+    return parking;
+  }
+
+  public Parking exit(String id) {
+    // recuperar o estacionado
+    // atualizar data de saida
+    // calcular o valor
+
+    return null;
   }
 
 }
